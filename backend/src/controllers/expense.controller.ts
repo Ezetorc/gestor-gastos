b86076444b9bead698b1763932757e0e4618375c
Ezetorc <ezetorc@gmail.com>
2025-07-11 15:15:29 -0300

    Added folders and files for initial structure and configuration

--- backend/src/controllers/expense.controller.ts
+++ backend/src/controllers/expense.controller.ts
@@ -1,10 +1,10 @@
-import { Response } from 'express'
-import { ExpenseService } from '../services/expense.service'
+import { Response } from "express";
+import { ExpenseModel } from "../models/expense.model";
 
 export class ExpenseController {
-  static async getAll (response: Response): Promise<void> {
-    const expenses = await ExpenseService.getAll()
+  static async getAll(response: Response): Promise<void> {
+    const expenses = await ExpenseModel.getAll();
 
-    response.json({ value: expenses })
+    response.json({ value: expenses });
   }
 }
--- backend/src/models/expense.model.ts
+++ backend/src/models/expense.model.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 
-export class ExpenseService {
+export class ExpenseModel {
   static prisma = new PrismaClient()
 
   static async getAll () {

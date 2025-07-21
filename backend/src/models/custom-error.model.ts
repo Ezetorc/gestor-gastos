c1c27ba3bc5fa2990ba9c1dbab8b40bb9baf367a
Ezetorc <ezetorc@gmail.com>
2025-07-21 15:16:27 -0300

    Crear NotFoundError como ejemplo de error personalizado

--- backend/src/models/custom-error.model.ts
+++ backend/src/models/custom-error.model.ts
@@ -1,16 +1,10 @@
 export class CustomError extends Error {
-  message: string;
-  statusCode: number;
-
-  constructor({
-    message,
-    statusCode,
-  }: {
-    message: string;
-    statusCode: number;
-  }) {
+  constructor(message: string, statusCode: number) {
     super();
+
     this.message = message;
     this.statusCode = statusCode;
   }
+
+  public statusCode: number;
 }

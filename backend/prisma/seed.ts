4e7fc52ea5d1e3db322db301bdfe13ef300d6e62
Ezetorc <ezetorc@gmail.com>
2025-07-24 16:25:55 -0300

    Añadir middleware de validación de DTO

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,49 +1,49 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
+import { prisma } from "../src/configuration/prisma.configuration";
 
-  //usuario de Alice
-  const alice = await prisma.user.upsert({
-    where: { email: 'alice@prisma.io' },
+async function main() {
+  // Usuarios
+  await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
     update: {},
     create: {
-      email: 'alice@prisma.io',
-      name: 'Alice',
-      password: 'securepassword123',
-      image: 'https://example.com/alice.jpg'
-
+      email: "alice@prisma.io",
+      name: "Alice",
+      password: "securepassword123",
+      image: "https://example.com/alice.jpg",
     },
-  })
-   //  Ingresos de Alice
-  const income = await prisma.income.create({
+  });
+
+  // Ingresos
+  await prisma.income.create({
     data: {
       amount: 5000,
-      date: new Date('2025-07-01'),
-      category: 'Salario',
-      paymentMethod: 'Transferencia',
-      description: 'Sueldo mensual'
+      date: new Date("2025-07-01"),
+      category: "Salario",
+      paymentMethod: "Transferencia",
+      description: "Sueldo mensual",
     },
-  })
+  });
 
-  //  Gastos de Alice
-  const expense = await prisma.expense.create({
+  // Gastos
+  await prisma.expense.create({
     data: {
       amount: 250,
-      date: new Date('2025-07-03'),
-      category: 'Supermercado',
-      paymentMethod: 'Efectivo',
-      description: 'Compras de alimentos'
+      date: new Date("2025-07-03"),
+      category: "Supermercado",
+      paymentMethod: "Efectivo",
+      description: "Compras de alimentos",
     },
-  })
-
-  console.log({ alice })
+  });
 }
+
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+  .catch(async (error) => {
+    console.error(error);
+
+    await prisma.$disconnect();
+
+    process.exit(1);
+  });
